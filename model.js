6aeda282372a50f35309327246d9200d5f038062
cyrilscott <37353502+cyrilscott@users.noreply.github.com>
2021-08-09 15:33:40 -0400

    Update model.js

--- model.js
+++ model.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 const User = new mongoose.Schema({
-    username: {type: String, required: true},
-    password: { type: String, required: true}
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    dateCreated: { type: String, required: true },
 })
 mongoose.models = {}
 module.exports = mongoose.model('UserModel', User)
